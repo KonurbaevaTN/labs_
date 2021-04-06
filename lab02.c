4fc76caf4c790c7eb2e5a186847093007089532f
KonurbaevaTN <konurbaeva_tn@edu.surgu.ru>
2021-04-06 14:52:32 +0500

    сделана лаба 2

--- lab02.c
+++ lab02.c
@@ -2,16 +2,16 @@
 #include <math.h>
 int main()
 {
-  double x;
-  printf("Enter x {(x <= -1) || (x > 1)} -> ");
-  scanf("%lf", &x);
-  if (x <= -1 || x > 1)
-  {
-    double y = (x-2)*sqrt((1+x)/(x-1));
-    double z = log (y*y-1);
-    printf ("y(x) = %lf\nz(y) = %lf\n", y, z);
-  }
-  else
-    printf("x value is incorrect!\n");
-  return 0;
+    double x;
+    printf("Enter x {(x < -1.4142135623730950488016887242097) || (x >= 3)} -> ");
+    scanf("%lf", &x);
+    if (x < -1.4142135623730950488016887242097 || x >= 3)
+    {
+        double y = (x - 2) * sqrt((1 + x) / (x - 1));
+        double z = log(y * y - 1);
+        printf ("y(x) = %lf\nz(y) = %lf\n", y, z);
+    }
+    else
+        printf("x value is incorrect!\n");
+    return 0;
 }
